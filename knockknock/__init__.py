c582934c8da9808b31342fca065de5d4f25b7cb7
VictorSanh <victorsanh@gmail.com>
2019-03-23 14:19:09 -0400

    fix typo

--- knockknock/__init__.py
+++ knockknock/__init__.py
@@ -1,3 +1,3 @@
 from knockknock.email_sender import email_sender
 from knockknock.slack_sender import slack_sender
-from knockknock.telegram_sender import telegrams_sender
+from knockknock.telegram_sender import telegram_sender
