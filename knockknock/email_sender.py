68b490eb75a3a53da4e888679726b5541782eea9
Victor SANH <victorsanh@gmail.com>
2019-05-13 15:04:47 -0400

    Distributed training (#11)
    * Add `RANK` information for distributed training.
    
    * Only master process sends notifications (except for errors)
    
    * Update README

--- knockknock/email_sender.py
+++ knockknock/email_sender.py
@@ -1,3 +1,4 @@
+import os
 import datetime
 import traceback
 import functools
@@ -29,31 +30,46 @@ def email_sender(recipient_email: str, sender_email: str = None):
             start_time = datetime.datetime.now()
             host_name = socket.gethostname()
             func_name = func.__name__
-            contents = ['Your training has started.',
-                        'Machine name: %s' % host_name,
-                        'Main call: %s' % func_name,
-                        'Starting date: %s' % start_time.strftime(DATE_FORMAT)]
-            yag_sender.send(recipient_email, 'Training has started 🎬', contents)
+
+            # Handling distributed training edge case.
+            # In PyTorch, the launch of `torch.distributed.launch` sets up a RANK environment variable for each process.
+            # This can be used to detect the master process.
+            # See https://github.com/pytorch/pytorch/blob/master/torch/distributed/launch.py#L211
+            # Except for errors, only the master process will send notifications.
+            if 'RANK' in os.environ:
+                master_process = (int(os.environ['RANK']) == 0)
+                host_name += ' - RANK: %s' % os.environ['RANK']
+            else:
+                master_process = True
+
+            if master_process:
+                contents = ['Your training has started.',
+                            'Machine name: %s' % host_name,
+                            'Main call: %s' % func_name,
+                            'Starting date: %s' % start_time.strftime(DATE_FORMAT)]
+                yag_sender.send(recipient_email, 'Training has started 🎬', contents)
 
             try:
                 value = func(*args, **kwargs)
-                end_time = datetime.datetime.now()
-                elapsed_time = end_time - start_time
-                contents = ["Your training is complete.",
-                            'Machine name: %s' % host_name,
-                            'Main call: %s' % func_name,
-                            'Starting date: %s' % start_time.strftime(DATE_FORMAT),
-                            'End date: %s' % end_time.strftime(DATE_FORMAT),
-                            'Training duration: %s' % str(elapsed_time)]
 
+                if master_process:
+                    end_time = datetime.datetime.now()
+                    elapsed_time = end_time - start_time
+                    contents = ["Your training is complete.",
+                                'Machine name: %s' % host_name,
+                                'Main call: %s' % func_name,
+                                'Starting date: %s' % start_time.strftime(DATE_FORMAT),
+                                'End date: %s' % end_time.strftime(DATE_FORMAT),
+                                'Training duration: %s' % str(elapsed_time)]
 
-                try:
-                    str_value = str(value)
-                    contents.append('Main call returned value: %s'% str_value)
-                except:
-                    contents.append('Main call returned value: %s'% "ERROR - Couldn't str the returned value.")
+                    try:
+                        str_value = str(value)
+                        contents.append('Main call returned value: %s'% str_value)
+                    except:
+                        contents.append('Main call returned value: %s'% "ERROR - Couldn't str the returned value.")
 
-                yag_sender.send(recipient_email, 'Training has sucessfully finished 🎉', contents)
+                    yag_sender.send(recipient_email, 'Training has sucessfully finished 🎉', contents)
+
                 return value
 
             except Exception as ex:
--- knockknock/telegram_sender.py
+++ knockknock/telegram_sender.py
@@ -1,3 +1,4 @@
+import os
 import datetime
 import traceback
 import functools
@@ -30,32 +31,48 @@ def telegram_sender(token: str, chat_id: int):
             start_time = datetime.datetime.now()
             host_name = socket.gethostname()
             func_name = func.__name__
-            contents = ['Your training has started 🎬',
-                        'Machine name: %s' % host_name,
-                        'Main call: %s' % func_name,
-                        'Starting date: %s' % start_time.strftime(DATE_FORMAT)]
-            text = '\n'.join(contents)
-            bot.send_message(chat_id=chat_id, text=text)
+
+            # Handling distributed training edge case.
+            # In PyTorch, the launch of `torch.distributed.launch` sets up a RANK environment variable for each process.
+            # This can be used to detect the master process.
+            # See https://github.com/pytorch/pytorch/blob/master/torch/distributed/launch.py#L211
+            # Except for errors, only the master process will send notifications.
+            if 'RANK' in os.environ:
+                master_process = (int(os.environ['RANK']) == 0)
+                host_name += ' - RANK: %s' % os.environ['RANK']
+            else:
+                master_process = True
+
+            if master_process:
+                contents = ['Your training has started 🎬',
+                            'Machine name: %s' % host_name,
+                            'Main call: %s' % func_name,
+                            'Starting date: %s' % start_time.strftime(DATE_FORMAT)]
+                text = '\n'.join(contents)
+                bot.send_message(chat_id=chat_id, text=text)
 
             try:
                 value = func(*args, **kwargs)
-                end_time = datetime.datetime.now()
-                elapsed_time = end_time - start_time
-                contents = ["Your training is complete 🎉",
-                            'Machine name: %s' % host_name,
-                            'Main call: %s' % func_name,
-                            'Starting date: %s' % start_time.strftime(DATE_FORMAT),
-                            'End date: %s' % end_time.strftime(DATE_FORMAT),
-                            'Training duration: %s' % str(elapsed_time)]
 
-                try:
-                    str_value = str(value)
-                    contents.append('Main call returned value: %s'% str_value)
-                except:
-                    contents.append('Main call returned value: %s'% "ERROR - Couldn't str the returned value.")
+                if master_process:
+                    end_time = datetime.datetime.now()
+                    elapsed_time = end_time - start_time
+                    contents = ["Your training is complete 🎉",
+                                'Machine name: %s' % host_name,
+                                'Main call: %s' % func_name,
+                                'Starting date: %s' % start_time.strftime(DATE_FORMAT),
+                                'End date: %s' % end_time.strftime(DATE_FORMAT),
+                                'Training duration: %s' % str(elapsed_time)]
 
-                text = '\n'.join(contents)
-                bot.send_message(chat_id=chat_id, text=text)
+                    try:
+                        str_value = str(value)
+                        contents.append('Main call returned value: %s'% str_value)
+                    except:
+                        contents.append('Main call returned value: %s'% "ERROR - Couldn't str the returned value.")
+
+                    text = '\n'.join(contents)
+                    bot.send_message(chat_id=chat_id, text=text)
+
                 return value
 
             except Exception as ex:
