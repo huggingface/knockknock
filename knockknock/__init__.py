cadce197c473fe263749fe1ac8f52a2b3d7d327b
abhishekkrthakur <abhishek4@gmail.com>
2019-09-15 13:11:32 +0200

    add sms sender

--- knockknock/__init__.py
+++ knockknock/__init__.py
@@ -1,4 +1,5 @@
 from knockknock.email_sender import email_sender
 from knockknock.slack_sender import slack_sender
+from knockknock.sms_sender import sms_sender
 from knockknock.telegram_sender import telegram_sender
 from knockknock.teams_sender import teams_sender
